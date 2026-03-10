07412442d770da3e7892fbd5a0d0978e084d5cbd
keysha-adesi <keyshaputrii590@gmail.com>
2026-03-10 18:44:32 +0700

    lolos

---
+++ pemdas/file2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata( float a, float b){
+    return (a+b)/2;
+}
+
+string Status_lulus(float r){
+    if (r >= 60)
+    return "Lolos";
+    else if (r >= 50)
+    return "Lolos dipertimbangkan";
+    else if ( r >= 40)
+    return "Lolos tetapi matrikulasi 1 tahun";
+    else 
+    return "Gagal";
+}
+
+int main(){
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    //rerata = (nilB+nilMT)/2;
+    //if (rerata >= 60)
+    //   status "Lolos";
+    //else
+    //  status"Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = status_lulus(rerata);
+
+    cout << "status kelulusan = "<<Status_lulus(rerata)<<"  ,degan nilai rerata = "<<rerata<<endl;
+    
+}
